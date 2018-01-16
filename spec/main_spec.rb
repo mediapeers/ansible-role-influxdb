ca1bf4a0d74d262ce967aa3a4b0be6b610731774
Stefan Horning <stefan@hornings.de>
2018-01-16 12:56:20 +0100

    Enable idempotent test again. Added more server specs to role testing

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 describe 'InfluxDB setup' do
   describe package('influxdb') do
     it { should be_installed }
@@ -16,7 +17,18 @@ describe 'InfluxDB setup' do
 end
 
 describe 'Chronograf setup' do
-  # TODO
+  describe package('chronograf') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/default/chronograf') do
+    it { should be_file }
+    its(:content) { should include("CHRONOGRAF_OPTS=\"--public-url #{ANSIBLE_VARS.fetch('chronograf_public_url', 'FAIL')}") }
+    its(:content) { should include("--token-secret #{ANSIBLE_VARS.fetch('influxdb_chronograf_oauth_secret', 'FAIL')}") }
+    its(:content) { should include("--github-client-id #{ANSIBLE_VARS.fetch('influxdb_chronograf_oauth_github_id', 'FAIL')}") }
+    its(:content) { should include("--github-client-secret #{ANSIBLE_VARS.fetch('influxdb_chronograf_oauth_github_secret', 'FAIL')}") }
+    its(:content) { should include("--github-organization #{ANSIBLE_VARS.fetch('influxdb_chronograf_oauth_github_org', 'FAIL')}") }
+  end
 
   describe service('chronograf') do
     it { should be_enabled }
@@ -24,7 +36,17 @@ describe 'Chronograf setup' do
 end
 
 describe 'Kapacitor setup' do
-  # TODO
+  describe package('kapacitor') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/kapacitor/kapacitor') do
+    it { should be_file }
+    its(:content) { should include("enabled = true") }
+    its(:content) { should include("urls = [\"http://#{ANSIBLE_VARS.fetch('influxdb_host', 'FAIL')}:#{ANSIBLE_VARS.fetch('influxdb_port', 'FAIL')}\"]") }
+    its(:content) { should include("username = #{ANSIBLE_VARS.fetch('influxdb_admin_user', 'FAIL')}") }
+    its(:content) { should include("password = #{ANSIBLE_VARS.fetch('influxdb_admin_pw', 'FAIL')}") }
+  end
 
   describe service('kapacitor') do
     it { should be_enabled }
